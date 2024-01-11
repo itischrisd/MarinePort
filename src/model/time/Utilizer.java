6b6fdc80cb0d132857e2f343848005f8858cbbb8
Krzysztof Dudek <dudek.krzysztof94@gmail.com>
2024-01-11 15:36:52 +0100

    little work on Utilizer

--- src/model/time/Utilizer.java
+++ src/model/time/Utilizer.java
@@ -1,4 +1,23 @@
 package model.time;
 
-public class Utilizer {
+import java.time.LocalDate;
+
+public class Utilizer extends Thread {
+
+    private final Clock clock;
+
+    public Utilizer(Clock clock) {
+        this.clock = clock;
+    }
+
+    @Override
+    public void run() {
+        while (!interrupted()) {
+            try {
+                LocalDate localDate = clock.getDate();
+            } catch (NullPointerException e) {
+                break;
+            }
+        }
+    }
 }
